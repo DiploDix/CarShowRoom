91b3e6c07fb8ba49be8f9a82e56e4e0951bab7b0
DiploDix <albertyaman98@gmail.com>
2017-10-26 23:31:45 +0300

    Carshowroom

---
+++ CarShowRoom/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarShowRoom
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Uri uriToSearchPage = new Uri("/View/Search.xaml", UriKind.Relative);
+        Uri uriToCarsPage = new Uri("/View/Cars.xaml", UriKind.Relative);
+        Uri uriToUsersPage = new Uri("/View/Users.xaml", UriKind.Relative);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* Переход во фрейме к странице /View/Search.xaml */
+        private void BorderSearch_Click(object sender, MouseButtonEventArgs e)
+        {
+            (((Border)sender).Child as TextBlock).Foreground = Brushes.Orange;
+            frameWindow.Source = uriToSearchPage;
+        }
+
+        /* Переход во фрейме к странице /View/Cars.xaml */
+        private void BorderCars_Click(object sender, MouseButtonEventArgs e)
+        {
+            (((Border)sender).Child as TextBlock).Foreground = Brushes.Orange;
+            frameWindow.Source = uriToCarsPage;
+        }
+
+        /* Переход во фрейме к странице /View/Users.xaml */
+        private void BorderUsers_Click(object sender, MouseButtonEventArgs e)
+        {
+            (((Border)sender).Child as TextBlock).Foreground = Brushes.Orange;
+            frameWindow.Source = uriToUsersPage;
+
+        }
+
+        private void TextBlock_Click(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ CarShowRoom/Model/Car.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace CarShowRoom.Model
+{
+    class Car : INotifyPropertyChanged
+    {
+        public enum StateCars
+        {
+            Отличное,
+            Хорошее,
+            Нормальное,
+            Удовлетворительно,
+            Ужасно
+        };
+
+        public enum AbroadCars
+        {
+            Отечественные,
+            Зарубежные
+        }
+
+        public enum EngineTypeCars
+        {
+            Бензин,
+            Дизель,
+            Электро,
+            Гибрид,
+            Газ
+        }
+
+        public enum TransmissionCars
+        {
+            Автомат,
+            Механика,
+            Типтроник,
+            Адаптивная,
+            Вариатор
+        }
+
+        public enum BodyTypeCars
+        {
+            Универсал,
+            Седан,
+            Хэтчбек,
+            Внедорожник,
+            Купе,
+            Кабриолет,
+            Минивэн,
+            Пикап
+        }
+
+        public enum RegionCars
+        {
+            Винницкая,
+            Волынская,
+            Днепропетровская,
+            Донецка,
+            Житомирская,
+            Закарпатская,
+            Запорожская,
+            Ивано_Франковская,
+            Киевская,
+            Кировоградская,
+            ЛуганскаяЛьвовская,
+            Николаевская,
+            Одесская,
+            Полтавска,
+            Республика_Крым,
+            Ровенская,
+            Сумская,
+            Тернопольская,
+            Харьковская,
+            Херсонская,
+            Хмельницкая,
+            Черкасская,
+            Черниговская,
+            Черновицкая
+        }
+
+        /* Состояние авто(бу, новое) */
+        public StateCars State { get; set; }
+
+        /* Отечественное или иностранное авто */
+        public AbroadCars AbroadCar { get; set; }
+
+        /* Марка авто */
+        public string MarkCar { get; set; }
+
+        /* Модель авто */
+        public string ModelCar { get; set; }
+
+        /* Цена грн */
+        public int PriceCar { get; set; }
+
+        /* Год выпуска */
+        public int YearCar { get; set; }
+
+        /* Обьем двигателя*/
+        public float EngineAmountCar { get; set; }
+
+        /* Тип двигателя */
+        public EngineTypeCars EngineTypeCar { get; set; }
+
+        /* КПП */
+        public TransmissionCars TransmissionCar { get; set; }
+
+        /* Тип кузова */
+        public BodyTypeCars BodyTypeCar { get; set; }
+
+        /* Область */
+        public RegionCars RegionCar { get; set; }
+
+        /* Город */
+        public string CityCar { get; set; }
+
+        public static ObservableCollection<Car> carsList = new ObservableCollection<Car>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
